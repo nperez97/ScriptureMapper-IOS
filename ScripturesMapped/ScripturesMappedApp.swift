594207f727fea48a93ac1a0e311451cea0c1f64d
Nefi Perez <nperez97@byu.edu>
2021-11-18 08:36:30 -0700

    Initial Commit

---
+++ ScripturesMapped/ScripturesMappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScripturesMappedApp.swift
+//  ScripturesMapped
+//
+//  Created by IS543 on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ScripturesMappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
