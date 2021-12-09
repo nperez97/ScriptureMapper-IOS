948162d79ae04a10d4ff806a00d9839616e11299
Nefi Perez <nperez97@byu.edu>
2021-12-09 16:07:15 -0700

    chapter "buttons"

--- ScripturesMapped/ChapterGridView.swift
+++ ScripturesMapped/ChapterGridView.swift
@@ -15,16 +15,23 @@ struct ChapterGridView: View {
         ScrollView {
             LazyVGrid(columns: Array(
                                     repeating: GridItem(.flexible(), spacing: 8),
-                                    count: 6
+                                    count: 5
                                 ),
                       spacing: 8) {
-                ForEach(1...(book.numChapters ?? 0), id: \.self) { chapter in
-                    NavigationLink("\(chapter)") {
-                        ChapterContentView(book: book, chapter: chapter)
+                            ForEach(1...(book.numChapters ?? 0), id: \.self) { chapter in
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 6)
+                                        .foregroundColor(.brown)
+                                        .shadow(radius: 2, x: 1, y: 1)
+                                    NavigationLink("\(chapter)") {
+                                        ChapterContentView(book: book, chapter: chapter)
+                                    }
+                                    .isDetailLink(false)
+                                    .padding(.vertical, 10)
+                                    .foregroundColor(.black)
+                                }
+                            }
                     }
-                    .isDetailLink(false)
-                }
-            }
         }
         .navigationTitle("\(book.fullName)")
     }
