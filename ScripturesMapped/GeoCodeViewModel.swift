9385040b10a69a4d77b81e57d5d0c64aa047a0ff
Nefi Perez <nperez97@byu.edu>
2021-12-08 19:18:01 -0700

    Started unique places code

--- ScripturesMapped/GeoCodeViewModel.swift
+++ ScripturesMapped/GeoCodeViewModel.swift
@@ -10,24 +10,46 @@ import Foundation
 class GeoCodeViewModel: ObservableObject, GeoPlaceCollector {
     
     @Published var geoPlaces = [GeoPlace]()
+    @Published var isDetailViewVisible = false
     
     init() {
         //ScriptureRenderer.shared.injectGeoPlaceCollector(self)
     }
     
     func setGeocodedPlaces(_ places: [GeoPlace]?) {
+        var newPlaces = [GeoPlace]() // new empty array of unique places
+        let delta = 0.00000001
+        
+        
         if let places = places {
             // TODO: only copy over the unique geoplaces
-            
-            
-            
             geoPlaces = places
+        }
+        
+        geoPlaces.forEach{ place in
             
-            geoPlaces.forEach{ place in
-                print(place.placename)
+            //compare latitdues and longititude. check if two pins are super close to each other
+            if let index = newPlaces.firstIndex(where: { abs($0.latitude - place.latitude) < delta && abs($0.longitude - place.longitude) < delta }) {
+                // if place already added, then add new name
+                if !newPlaces[index].placename.contains(place.placename) {
+                    newPlaces[index].placename.append(", \(place.placename)")
+                }
+            } else {
+                newPlaces.append(place)
             }
         }
+        
+        geoPlaces = newPlaces
     }
+    
+    func setCurrentGeoPlace(placeId: Int) {
+        //TODO
+    }
+    
+    func setRegion(geoPlaces: [GeoPlace]) {
+        //TODO
+    }
+    
     
 }
 
